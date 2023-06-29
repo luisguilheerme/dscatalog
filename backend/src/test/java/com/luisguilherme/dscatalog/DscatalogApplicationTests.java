a7211ecfd4d368181cce47b57908219fe2f9129a
luisguilheerme <lg.maroni@gmail.com>
2023-06-29 19:14:53 -0300

    Project created

---
+++ backend/src/test/java/com/luisguilherme/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisguilherme.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
